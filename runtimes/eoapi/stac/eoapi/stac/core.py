ad9ed5ef593c79077fe534f09bed9e7fc755581d
LAMBARE Aubin <aubin.lambare@csgroup.eu>
2025-01-13 15:40:31 +0100

    fix: search pagination with pgstac >= 0.9.0

--- runtimes/eoapi/stac/eoapi/stac/core.py
+++ runtimes/eoapi/stac/eoapi/stac/core.py
@@ -19,20 +19,23 @@ import json
 import logging
 import re
 import time
-from typing import Any, Callable, Coroutine, Dict, List, Optional, TypeVar
+from typing import Any, Callable, Coroutine, Dict, List, Optional, Set, TypeVar
 from urllib.parse import unquote_plus, urljoin
 
 import attr
 import orjson
+from asyncpg.exceptions import InvalidDatetimeFormatError
 from buildpg import render
 from fastapi import Depends, Request
 from pygeofilter.backends.cql2_json import to_cql2
 from pygeofilter.parsers.cql2_text import parse as parse_cql2_text
+from pypgstac.hydration import hydrate
 from stac_fastapi.pgstac.core import CoreCrudClient
-from stac_fastapi.pgstac.models.links import CollectionLinks
+from stac_fastapi.pgstac.models.links import CollectionLinks, ItemLinks, PagingLinks
 from stac_fastapi.pgstac.types.search import PgstacSearch
-from stac_fastapi.pgstac.utils import format_datetime_range
+from stac_fastapi.pgstac.utils import filter_fields, format_datetime_range
 from stac_fastapi.types.core import Relations
+from stac_fastapi.types.errors import InvalidQueryParameter
 from stac_fastapi.types.requests import get_base_url
 from stac_fastapi.types.rfc3339 import DateTimeType
 from stac_fastapi.types.stac import (
@@ -240,6 +243,120 @@ class EOCClient(CoreCrudClient):
         result.pop("scope", None)
         return result
 
+    async def _base_search_base(  # noqa: C901
+        self,
+        search_request: PgstacSearch,
+        request: Request,
+    ) -> ItemCollection:
+        """Cross catalog search (POST).
+
+        Called with `POST /search`.
+
+        Args:
+            search_request: search request parameters.
+
+        Returns:
+            ItemCollection containing items which match the search criteria.
+        """
+        items: Dict[str, Any]
+
+        settings: Settings = request.app.state.settings
+
+        if search_request.datetime:
+            search_request.datetime = format_datetime_range(search_request.datetime)
+
+        search_request.conf = search_request.conf or {}
+        search_request.conf["nohydrate"] = settings.use_api_hydrate
+
+        search_request_json = search_request.model_dump_json(exclude_none=True, by_alias=True)
+
+        try:
+            async with request.app.state.get_connection(request, "r") as conn:
+                q, p = render(
+                    """
+                    SELECT * FROM search(:req::text::jsonb);
+                    """,
+                    req=search_request_json,
+                )
+                items = await conn.fetchval(q, *p)
+        except InvalidDatetimeFormatError as e:
+            raise InvalidQueryParameter(f"Datetime parameter {search_request.datetime} is invalid.") from e
+
+        # Starting in pgstac 0.9.0, the `next` and `prev` tokens are returned in spec-compliant links with method GET
+        next_from_link: Optional[str] = None
+        prev_from_link: Optional[str] = None
+        for link in items.get("links", []):
+            if link.get("rel") == "next":
+                next_from_link = link.get("href").split("token=next:")[1]
+            if link.get("rel") == "prev":
+                prev_from_link = link.get("href").split("token=prev:")[1]
+
+        next: Optional[str] = items.pop("next", next_from_link)
+        prev: Optional[str] = items.pop("prev", prev_from_link)
+        collection = ItemCollection(**items)
+
+        fields = getattr(search_request, "fields", None)
+        include: Set[str] = fields.include if fields and fields.include else set()
+        exclude: Set[str] = fields.exclude if fields and fields.exclude else set()
+
+        async def _add_item_links(
+            feature: Item,
+            collection_id: Optional[str] = None,
+            item_id: Optional[str] = None,
+        ) -> None:
+            """Add ItemLinks to the Item.
+
+            If the fields extension is excluding links, then don't add them.
+            Also skip links if the item doesn't provide collection and item ids.
+            """
+            collection_id = feature.get("collection") or collection_id
+            item_id = feature.get("id") or item_id
+
+            if not exclude or "links" not in exclude and all([collection_id, item_id]):
+                feature["links"] = await ItemLinks(
+                    collection_id=collection_id,  # type: ignore
+                    item_id=item_id,  # type: ignore
+                    request=request,
+                ).get_links(extra_links=feature.get("links"))
+
+        cleaned_features: List[Item] = []
+
+        if settings.use_api_hydrate:
+
+            async def _get_base_item(collection_id: str) -> Dict[str, Any]:
+                return await self._get_base_item(collection_id, request=request)
+
+            base_item_cache = settings.base_item_cache(fetch_base_item=_get_base_item, request=request)
+
+            for feature in collection.get("features") or []:
+                base_item = await base_item_cache.get(feature.get("collection"))
+                # Exclude None values
+                base_item = {k: v for k, v in base_item.items() if v is not None}
+
+                feature = hydrate(base_item, feature)
+
+                # Grab ids needed for links that may be removed by the fields extension.
+                collection_id = feature.get("collection")
+                item_id = feature.get("id")
+
+                feature = filter_fields(feature, include, exclude)
+                await _add_item_links(feature, collection_id, item_id)
+
+                cleaned_features.append(feature)
+        else:
+            for feature in collection.get("features") or []:
+                await _add_item_links(feature)
+                cleaned_features.append(feature)
+
+        collection["features"] = cleaned_features
+        collection["links"] = await PagingLinks(
+            request=request,
+            next=next,
+            prev=prev,
+        ).get_links()
+
+        return collection
+
     async def _search_base(
         self,
         search_request: PgstacSearch,
@@ -260,7 +377,7 @@ class EOCClient(CoreCrudClient):
             search_request.collections = list(set(collections_with_scopes) & set(search_request.collections))
 
         async def _fetch() -> ItemCollection:
-            result = await _super._search_base(search_request, request=request)
+            result = await self._base_search_base(search_request, request=request)
 
             ts = time.perf_counter()
             item_collection = ItemCollection(**result)
